ed1dcffbe36c639a8cdb6c9d34d7702fd47deebb
Stijn van der Pol <s.o.vanderpol@gmail.com>
2020-01-18 21:16:40 +0100

    add a searchTweet function to search for tweets and map the collection

--- app/Services/TwitterService.php
+++ app/Services/TwitterService.php
@@ -2,9 +2,35 @@
 
 namespace App\Services;
 
+use App\Clients\TwitterClient;
+use App\Utils\HashtagCollector;
+
 class TwitterService {
 
-    public function __construct() {
+    private $client;
 
+    public function __construct() {
+        $this->client = new TwitterClient(); 
+    }
+
+    public function searchTweet($searchString) {
+        $searchResults = $this->client->search($searchString);
+        $tweets = [ 'tweets' => [], 'meta' => [] ];
+        $hashtagCollector = new HashtagCollector();
+
+        foreach($searchResults->statuses as $tweet) {
+            $hashtagCollector->getHashtagsFromTweet($tweet);
+            array_push($tweets['tweets'], [
+                'id' => $tweet->id_str,
+                'name' => $tweet->user->name,
+                'handle' => $tweet->user->screen_name,
+                'profile_picture' => $tweet->user->profile_image_url_https,
+                'contents' => $tweet->text
+            ]);
+        }
+        
+        $tweets['meta']['top10Hashtags'] = $hashtagCollector->getTop10Hashtags();
+
+        return $tweets;
     }
 }
