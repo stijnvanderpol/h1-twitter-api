f3c1bd4a585c2250a5abb3184449d89ef30c3b98
Stijn van der Pol <s.o.vanderpol@gmail.com>
2020-01-18 21:24:41 +0100

    add a search handler to the twitter controller

--- app/Http/Controllers/TwitterController.php
+++ app/Http/Controllers/TwitterController.php
@@ -2,10 +2,26 @@
 
 namespace App\Http\Controllers;
 
+use App\Services\TwitterService;
+use Illuminate\Http\Request;
+
+
 class TwitterController extends Controller
 {
+
+    private $service;
+
     public function __construct()
     {
-        //
+        $this->service = new TwitterService();
+    }
+
+    public function search(Request $request) {
+        $this->validate($request, [
+            'query' => 'required|max:100'
+        ]);
+        
+        $tweets = $this->service->searchTweet($request->input('query'));
+        return response()->json($tweets);
     }
 }
